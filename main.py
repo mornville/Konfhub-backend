6b3be1b5b34df86bb7c33972930eef8e0b1525ab
Ashutosh <aj97389@gmail.com>
2020-06-12 22:03:05 +0530

    task1 completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 url = 'https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences'
 data = {}
 combinedData = []
-class api_wrapper:
+class Wrapper:
     #Fetching data from REST Api
     def getData(self, url)->dict:
         r = requests.get(url)
@@ -19,11 +19,10 @@ class api_wrapper:
             combinedData.append(i)       
         for i in data['free']:
             combinedData.append(i)       
-        print(len(combinedData))
     
     #TASK 1: Print data in human readable format
     def task1(self, data)->list:
-        count = 0
+        count = 1
         for entry in data:
             #Preprocessing conferenece start date to convert into format like November 6th, 2020
             if(int(entry['confStartDate'].split(' ')[0])<10):
@@ -33,10 +32,14 @@ class api_wrapper:
                 day = entry['confStartDate'].split(' ')[0] + "th"
             else:
                 day = entry['confStartDate'].split(' ')[0] + ["st", "nd", "rd"][int(entry['confStartDate'].split(' ')[0]) % 10 - 1]
+            #Combining date, month and year after adding suffix to the day
             startDate = str(entry['confStartDate'].split(' ')[1].replace(',', '') + ' ' + day + ', ' + entry['confStartDate'].split(' ')[2])
-            print() 
-            print(str(count) + str(entry['confName']) + ', ' + startDate)
-a = api_wrapper()
+            #printing single entry
+            print(str(count) + '. ' + str(entry['confName']) + ', ' + startDate + ', ' + str(entry['city']) + ', ' +str(entry['state']) + ', ' +  str(entry['country']) + ', ' + str(entry['entryType']) + ', ' +str(entry['confUrl'])  + '\n')
+            count+=1
+            
+#Declaring instance of class Wrapper
+a = Wrapper()
 data = a.getData(url)
 # print(data)
 if(data!= {}):
