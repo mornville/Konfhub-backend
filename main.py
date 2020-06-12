fe94dae9bb575da9be39c25578ff1961c482f69d
Ashutosh <aj97389@gmail.com>
2020-06-12 22:54:00 +0530

    Task2 exact duplicates done

--- main.py
+++ main.py
@@ -14,30 +14,46 @@ class Wrapper:
         else:
             return {}
     #Combining free and paid events
-    def combinePaidAndFree(self, data)->dict:
-        for i in data['paid']:
-            combinedData.append(i)       
-        for i in data['free']:
-            combinedData.append(i)       
+    def combinePaidAndFree(self, data)->dict:    
+        combinedData.extend(data['paid'])   
+        combinedData.extend(data['free'])       
     
-    #TASK 1: Print data in human readable format
-    def task1(self, data)->list:
+    #To Print individual entries
+    def printEntries(self, data):
         count = 1
         for entry in data:
             #Preprocessing conferenece start date to convert into format like November 6th, 2020
             if(int(entry['confStartDate'].split(' ')[0])<10):
                 entry['confStartDate'] = entry['confStartDate'][1:]
-
             if 4 <= int(entry['confStartDate'].split(' ')[0]) <= 20 or 24 <= int(entry['confStartDate'].split(' ')[0]) <= 30:
                 day = entry['confStartDate'].split(' ')[0] + "th"
             else:
                 day = entry['confStartDate'].split(' ')[0] + ["st", "nd", "rd"][int(entry['confStartDate'].split(' ')[0]) % 10 - 1]
             #Combining date, month and year after adding suffix to the day
             startDate = str(entry['confStartDate'].split(' ')[1].replace(',', '') + ' ' + day + ', ' + entry['confStartDate'].split(' ')[2])
+
             #printing single entry
             print(str(count) + '. ' + str(entry['confName']) + ', ' + startDate + ', ' + str(entry['city']) + ', ' +str(entry['state']) + ', ' +  str(entry['country']) + ', ' + str(entry['entryType']) + ', ' +str(entry['confUrl'])  + '\n')
             count+=1
+
+    #TASK 1: Print data in human readable format
+    def task1(self, data):
+        self.printEntries(data)
             
+
+    #TASK 2: Identify exact duplicates
+    def task2(self, data):
+        duplicates = []
+        for i in range(len(data) - 1):
+            if data[i] in data[i + 1 :]:
+                duplicates.append(data[i])
+            else:
+                pass
+        print('\nExact Duplicates: \n')
+        for entry in duplicates:
+            print(str(entry['confName']) + ', ' + entry['confStartDate'] + ', ' + str(entry['city']) + ', ' +str(entry['state']) + ', ' +  str(entry['country']) + ', ' + str(entry['entryType']) + ', ' +str(entry['confUrl'])  + '\n')
+      
+
 #Declaring instance of class Wrapper
 a = Wrapper()
 data = a.getData(url)
@@ -45,5 +61,6 @@ data = a.getData(url)
 if(data!= {}):
     a.combinePaidAndFree(data)
     a.task1(combinedData)
+    a.task2(combinedData)
 else:
     print('Could not fetch data')
